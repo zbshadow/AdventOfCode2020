c569fa7803adc1982d1fe210a7081a43ba255a3f
zbshadow <zbshadowx@gmail.com>
2020-12-02 01:00:23 -0500

    initial commit

---
+++ AoCDay2Part2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace AoCDay2Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "E:\\Programs\\AdventOfCode\\AoCDay2Part2\\input.txt";
+            string line;
+            List<string> lines = new List<string>();
+            List<string> ValidPass = new List<string>();
+
+            System.IO.StreamReader file = new System.IO.StreamReader(input);
+            while((line = file.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+
+            Console.WriteLine(lines.Count);
+
+            foreach (var l in lines)
+            {
+                string x = l.Trim(' ');
+                string[] parts = SplitLine(x);
+
+                //foreach(var part in parts)
+                //{
+                //    Console.WriteLine(part);
+                //}
+
+                int min = Int32.Parse(parts[0]);
+                int max = Int32.Parse(parts[1]);
+                string req = parts[2];
+                string pass = parts[4];
+
+                int count = 0;
+
+                foreach(char y in pass)
+                {
+                    if (y == req[0])
+                    {
+                        count++;
+                    }
+                }
+
+                if (count <= max && count >= min)
+                    ValidPass.Add(pass);
+            }
+
+            Console.WriteLine(ValidPass.Count);
+            
+        }
+
+        static string[] SplitLine(string s)
+        {
+            char[] breaks = { ':', '-', ' ' };
+
+            string[] parts = s.Split(breaks);
+
+            return parts;
+        }
+    }
+}
---
+++ AoCDayTwo/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SortAndFindSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //array
+            List<int> inputs = new List<int>();
+            string line;
+            string inputsFile = "E:\\Programs\\SortAndFindSum\\AdventofCodeDayTwo\\input.txt";
+
+            System.IO.StreamReader file = new System.IO.StreamReader(inputsFile);
+            while ((line = file.ReadLine()) != null)
+            {
+                inputs.Add(Int32.Parse(line));
+            }
+
+            file.Close();
+
+            //sort
+            inputs.Sort();
+            bool goal = false;
+            int a = 0;
+            int b = 1;
+            int c = 2;
+
+            while (goal == false)
+            {
+                goal = equalsGoal(inputs[a], inputs[b], inputs[c]);
+                if (goal)
+                {
+                    Console.WriteLine($"{a} + {b} + {c} are equal to 2020", inputs[a], inputs[b], inputs[c]);
+                    int winner = inputs[a] * inputs[b] * inputs[c];
+                    Console.WriteLine("Your winning value is " + winner);
+                }
+
+                if (c < inputs.Count - 1)
+                {
+                    c++;
+                }
+                else
+                {
+                    c = b + 1;
+                    if (b < inputs.Count - 1)
+                    {
+                        b++;
+                    }
+                    else
+                    {
+                        b = a + 1;
+                        a++;
+                    }
+                }
+            }
+
+            //find the sum of two elements that equals 2020
+            bool equalsGoal(int x, int y, int z)
+            {
+                int n = 2020;
+                int sum = x + y + z;
+
+                if (sum == n)
+                    return true;
+                else
+                    return false;
+            }
+
+
+            //if sum is 2020 then multiply
+        }
+    }
+}
