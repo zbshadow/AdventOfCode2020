2848b8190797c0a35cd95af798843114c563ca55
zbshadow <zbshadowx@gmail.com>
2020-12-06 20:00:20 -0500

    solution for Day 6 Part 1

--- AoCDay6/Program.cs
+++ AoCDay6/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace AoCDay6
 {
@@ -6,7 +8,31 @@ namespace AoCDay6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string file = @"E:\Programs\AdventOfCode\AoCDay6\input.txt";
+            string line;
+            int sum = 0;
+
+            StreamReader input = new StreamReader(file);
+            List<char> x = new List<char>();
+
+            while ((line = input.ReadLine()) != null)
+            {
+                if (line.Length > 0)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if (!(x.Contains(line[i])))
+                            x.Add(line[i]);
+                    }
+                }
+                else
+                {
+                    sum = sum + x.Count;
+                    x.Clear();
+                }
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
